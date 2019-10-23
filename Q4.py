cdfaa2e675015a11e68df589ef191cad8d2479bf
melikasp <galexy_p2008@yahoo.com>
2019-10-23 06:11:40 -0700

    update for question 4

--- Q4.py
+++ Q4.py
@@ -33,23 +33,30 @@ if __name__ == '__main__':
                 
                 
     # calculation of the ratio and showing it with players shortnames and their wyId
-    # Build height-ratio table
     player_ds = pd.read_json('../DS/players.json')
     
     Player_list = []
     for i in range(len(player_ds)):
         player_id = player_ds['wyId'][i]
         short_name = player_ds['shortName'][i]
-        #using the threshold
-        if data >= threshold: 
-            if player_id in player_performance.keys():
-                successful = player_performance[player_id][0]
-                failed = player_performance[player_id][1]
-                
-                ratio = 0 
-                if failed != 0:
-                    ratio = successful / (successful + failed)
-                Player_list.append([short_name, ratio])
+        
+        player_data = data.loc[data.playerId == player_id , "tags"]
+
+        
+        if player_data is not None and len(player_data) > 0:      
+            number_passes = player_data.tolist()[0]
+            
+            #using the threshold
+            if number_passes >= threshold: 
+                if player_id in player_performance.keys():
+                    successful = player_performance[player_id][0]
+                    failed = player_performance[player_id][1]
+                    
+                    ratio = 0 
+                    if failed != 0:
+                        ratio = successful / (successful + failed)
+                    Player_list.append([short_name, ratio])
+                    
     #reversing the list and sorting it to find the 10 first players with high ratio     
     Player_list.sort(key=operator.itemgetter(1))
     Player_list.reverse()
